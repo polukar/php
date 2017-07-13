0614e875e93f6480f065921011b9a57d9f1fb2c6
Artem Polukarov <apol@makler.fit>
2017-07-13 18:18:03 +0300

    work#1-5

--- #1/index.php
+++ #1/index.php
@@ -48,3 +48,30 @@
 		  echo "Неизвестный   возраст";
 	}
 ?>
+
+
+<?php
+	#Задание 1 пункт 5
+	$day = mt_rand(1, 10);
+	echo "<br><br><br>" . $day . "<br>";
+	switch ($day){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			echo "Это   рабочий   день";
+			break;
+		case 6:
+		case 7:
+			echo "Это   выходной   день";
+			break;
+		case 8:
+		case 9:
+		case 10:
+				echo "Неизвестный   день";
+				break;
+	}	
+
+	
+?>
